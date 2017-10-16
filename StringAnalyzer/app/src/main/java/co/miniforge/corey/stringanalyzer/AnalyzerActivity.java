d7ae79cc4ed934cf4a4ef809e43313af0a14ecff
h4r3krsna <manju.sreedaran@gmail.com>
2017-10-15 18:29:11 -0700

    All metrics now display data

--- StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
+++ StringAnalyzer/app/src/main/java/co/miniforge/corey/stringanalyzer/AnalyzerActivity.java
@@ -64,6 +64,8 @@ public class AnalyzerActivity extends AppCompatActivity {
         characterCount.setText(String.format("Character Count: %d", textUtil.getTextCharacterCount()));
         wordCount.setText(String.format("Word Count: %d", textUtil.getWordCount()));
         uniqueCharacters.setText(String.format("Unique Characters: %d", textUtil.getUniqueCharacters()));
+        uniqueWords.setText(String.format("Unique Words: %s", textUtil.getUniqueWords()));
+        longestWord.setText(String.format("Longest Word: %s", textUtil.getLongestWord()));
         specialCharactersCount.setText(String.format("Special Characters: %d", textUtil.getSpecialCharacterCount()));
 
         backToInputButton.setOnClickListener(new View.OnClickListener() {
